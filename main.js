79f24bfc332548294e7ca7d0d424804f1228e1cb
Murillo de Morais Souza <moraissouzamurillo@gmail.com>
2026-05-21 19:09:08 -0300

    atv2 lab4

---
+++ main.js
@@ -0,0 +1,81 @@
+const { RequestManager } = require("./requestManager");
+
+const rm = RequestManager.getInstance({
+  baseURL: "https://fakestoreapi.com",
+  headers: { "Test-Version": "2.1.0" },
+  maxConcurrent: 5,
+});
+
+rm.setAuthToken("meu-token-jwt");
+
+const userService = {
+  api: RequestManager.getInstance(), 
+  async listarUsuarios() {
+    return this.api.get("/users");
+  },
+
+  async criarUsuario(dados) {
+    return this.api.post("/users", dados);
+  },
+};
+
+const productService = {
+  api: RequestManager.getInstance(), 
+
+  async listarProdutos() {
+    return this.api.get("/products");
+  },
+
+  async atualizarProduto(id, dados) {
+    return this.api.put(`/products/${id}`, dados);
+  },
+};
+
+
+const orderService = {
+  api: RequestManager.getInstance(), 
+  async criarPedido(carrinho) {
+    return this.api.post("/orders", carrinho);
+  },
+
+  async cancelarPedido(id) {
+    return this.api.delete(`/orders/${id}`);
+  },
+};
+
+console.log(
+  "userService.api === productService.api?",
+  userService.api === productService.api 
+);
+
+console.log(
+  "productService.api === orderService.api?",
+  productService.api === orderService.api 
+);
+
+console.log("Stats:", RequestManager.getInstance().getStats());
+
+module.exports = { userService, productService, orderService };
+
+// ── Testes
+async function executarChamadas() {
+  
+  console.log("\n Executando chamadas simultâneas...");
+  
+  try {
+    const promessas = [
+      userService.listarUsuarios(),
+      productService.listarProdutos()
+    ];
+    console.log("Status atual da fila:", RequestManager.getInstance().getStats());
+
+    const [usuarios, produtos] = await Promise.all(promessas);
+    
+    console.log(`\n Número de usuários encontrados ${usuarios.length}.`);
+    console.log(`\n Numero de produtos encontrados ${produtos.length}.`);
+  } catch (error) {
+    console.error("\nErro na requisição:", error.message);
+  }
+}
+
+executarChamadas();
